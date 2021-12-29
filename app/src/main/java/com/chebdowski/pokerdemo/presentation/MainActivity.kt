3b5c1663319e4544eb673345e9826d578b9864ff
Piotr Chebdowski <chebdowski@gmail.com>
2021-12-29 14:24:02 +0100

    Base project structure

---
+++ app/src/main/java/com/chebdowski/pokerdemo/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chebdowski.pokerdemo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chebdowski.pokerdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
