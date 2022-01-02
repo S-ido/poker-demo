3d9230278c00c1e432c02fce9dca5ef60ad03980
Piotr Chebdowski <chebdowski@gmail.com>
2022-01-02 22:50:25 +0100

    Add view binding

--- app/src/main/java/com/chebdowski/pokerdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/chebdowski/pokerdemo/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.chebdowski.pokerdemo.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chebdowski.pokerdemo.R
+import androidx.appcompat.app.AppCompatActivity
+import com.chebdowski.pokerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
